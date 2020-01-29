162ec00090d868d0fd8a474ad7db2fbcb42b79ff
ryeash <ryan.yeash@broadcom.com>
2020-01-29 07:22:20 -0700

    simplify index

--- doctor-runtime/src/main/java/vest/doctor/ProviderIndex.java
+++ doctor-runtime/src/main/java/vest/doctor/ProviderIndex.java
@@ -8,57 +8,62 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 final class ProviderIndex {
 
-    private final Node root = new Node();
-    private final Map<Class<?>, List<Node>> inverse = new HashMap<>(128);
+    private final Map<Class<?>, Map<String, DoctorProvider<?>>> primary = new HashMap<>();
+    private final Map<Class<?>, Map<String, List<DoctorProvider<?>>>> secondary = new HashMap<>();
     private final Map<Class<? extends Annotation>, Collection<DoctorProvider<?>>> annotationTypeToProvider = new HashMap<>(128);
-    private final Lock writeLock = new ReentrantLock();
     private int size = 0;
 
     void setProvider(DoctorProvider<?> provider) {
-        writeLock.lock();
-        try {
-            Node temp = root;
-            for (Class<?> type : provider.allProvidedTypes()) {
-                temp = temp.getOrCreate(type);
-                inverse.computeIfAbsent(type, v -> new ArrayList<>(3)).add(temp);
+        synchronized (this) {
+            // primary
+            Map<String, DoctorProvider<?>> qualifierToProvider = primary.computeIfAbsent(provider.type(), t -> new HashMap<>());
+            if (qualifierToProvider.containsKey(provider.qualifier())) {
+                throw new IllegalArgumentException("there is already a provider registered under: " + provider.qualifier() + ":" + provider.type());
             }
-            temp.putProvider(provider);
+            qualifierToProvider.put(provider.qualifier(), provider);
+
+            // secondary
+            for (Class<?> type : provider.allProvidedTypes()) {
+                Map<String, List<DoctorProvider<?>>> sub = secondary.computeIfAbsent(type, t -> new HashMap<>());
+                sub.computeIfAbsent(provider.qualifier(), q -> new ArrayList<>()).add(provider);
+            }
 
             for (Class<? extends Annotation> annotation : provider.allAnnotationTypes()) {
                 annotationTypeToProvider.computeIfAbsent(annotation, a -> new HashSet<>(16)).add(provider);
             }
             size++;
-        } finally {
-            writeLock.unlock();
         }
     }
 
     @SuppressWarnings("unchecked")
     <T> Optional<DoctorProvider<T>> getProvider(Class<T> type, String qualifier) {
-        return Optional.ofNullable(inverse.get(type))
-                .orElse(Collections.emptyList())
+        // check primary
+        DoctorProvider<?> doctorProvider = primary.getOrDefault(type, Collections.emptyMap()).get(qualifier);
+        if (doctorProvider != null) {
+            return Optional.of((DoctorProvider<T>) doctorProvider);
+        }
+
+        // fallback to secondary
+        return secondary.getOrDefault(type, Collections.emptyMap())
+                .getOrDefault(qualifier, Collections.emptyList())
                 .stream()
-                .map(n -> (DoctorProvider<T>) n.getProvider(qualifier))
-                .filter(Objects::nonNull)
+                .map(p -> (DoctorProvider<T>) p)
                 .findFirst();
     }
 
     @SuppressWarnings("unchecked")
     <T> Stream<DoctorProvider<T>> getProviders(Class<T> type) {
-        return Optional.ofNullable(inverse.get(type))
-                .orElse(Collections.emptyList())
-                .stream()
-                .flatMap(Node::getProviders)
-                .map(p -> (DoctorProvider<T>) p)
-                .distinct();
+        return Optional.ofNullable(secondary.get(type))
+                .map(Map::values)
+                .map(Collection::stream)
+                .orElse(Stream.empty())
+                .flatMap(Collection::stream)
+                .map(p -> (DoctorProvider<T>) p);
     }
 
     Stream<DoctorProvider<?>> getProvidersWithAnnotation(Class<? extends Annotation> type) {
@@ -66,65 +71,12 @@ final class ProviderIndex {
     }
 
     Stream<DoctorProvider<?>> allProviders() {
-        return root.getProviders();
+        return primary.values().stream()
+                .map(Map::values)
+                .flatMap(Collection::stream);
     }
 
     int size() {
         return size;
     }
-
-    private static final class Node {
-        private Map<Class<?>, Node> sub;
-        private Map<String, DoctorProvider<?>> providers;
-
-        void putProvider(DoctorProvider<?> provider) {
-            if (providers == null) {
-                providers = new HashMap<>(16);
-            }
-            if (providers.containsKey(provider.qualifier())) {
-                throw new IllegalArgumentException("there is already a provider registered under: " + provider.qualifier() + ":" + provider.type());
-            }
-            providers.put(provider.qualifier(), provider);
-        }
-
-        Node getOrCreate(Class<?> type) {
-            if (sub == null) {
-                sub = new HashMap<>(16);
-            }
-            return sub.computeIfAbsent(type, t -> new Node());
-        }
-
-        DoctorProvider<?> getProvider(String qualifier) {
-            if (providers != null && providers.containsKey(qualifier)) {
-                DoctorProvider<?> provider = providers.get(qualifier);
-                if (provider != null) {
-                    return provider;
-                }
-            }
-            if (sub != null) {
-                for (Node value : sub.values()) {
-                    DoctorProvider<?> provider = value.getProvider(qualifier);
-                    if (provider != null) {
-                        return provider;
-                    }
-                }
-            }
-            return null;
-        }
-
-        Stream<DoctorProvider<?>> getProviders() {
-            Stream<DoctorProvider<?>> prim = Stream.empty();
-            if (providers != null) {
-                prim = providers.values().stream();
-            }
-            Stream<DoctorProvider<?>> desc = Stream.empty();
-            if (sub != null) {
-                desc = sub.values()
-                        .stream()
-                        .flatMap(Node::getProviders);
-            }
-            return Stream.concat(prim, desc);
-        }
-    }
-
 }
