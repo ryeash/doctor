b984d5325c4844a285841d6ad1c709e14f380396
ryeash <ryan.yeash@broadcom.com>
2021-04-05 13:44:11 -0600

    updates

--- doctor-processor/src/main/java/vest/doctor/processor/ShutdownCustomizationPoint.java
+++ doctor-processor/src/main/java/vest/doctor/processor/ShutdownCustomizationPoint.java
@@ -8,29 +8,32 @@ import vest.doctor.ProviderDefinition;
 import vest.doctor.codegen.MethodBuilder;
 import vest.doctor.codegen.ProcessorUtils;
 
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+
 public class ShutdownCustomizationPoint implements NewInstanceCustomizer {
 
     @Override
     public void customize(AnnotationProcessorContext context, ProviderDefinition providerDefinition, MethodBuilder method, String instanceRef, String providerRegistryRef) {
         if (providerDefinition.annotationSource().getAnnotation(DestroyMethod.class) != null) {
             DestroyMethod destroy = providerDefinition.annotationSource().getAnnotation(DestroyMethod.class);
-            if (!validMethod(context, providerDefinition, destroy.value())) {
-                throw new CodeProcessingException("invalid destroy method `" + providerDefinition.providedType() + "." + destroy.value() + "` is not valid; destroy methods must exist and have zero arguments");
+            String destroyMethod = destroy.value();
+            for (TypeElement type : providerDefinition.getAllProvidedTypes()) {
+                for (ExecutableElement m : ProcessorUtils.allMethods(context, type)) {
+                    if (m.getModifiers().contains(Modifier.PUBLIC) && m.getSimpleName().toString().equals(destroyMethod) && m.getParameters().size() == 0) {
+                        String closer = "((" + type.getSimpleName() + ")" + instanceRef + ")::" + destroy.value();
+                        method.line("{{providerRegistry}}.shutdownContainer().register(", closer, ");");
+                        return;
+                    }
+                }
             }
-            String closer = instanceRef + "::" + destroy.value();
-            method.line("{{providerRegistry}}.shutdownContainer().register(", closer, ");");
+            throw new CodeProcessingException("invalid destroy method `" + providerDefinition.providedType() + "." + destroy.value() + "` is not valid; destroy methods must exist, be public, and have zero arguments");
         } else {
             method.addImportClass(AutoCloseable.class);
             method.line("if(", instanceRef, " instanceof ", AutoCloseable.class, "){");
             method.line("{{providerRegistry}}.shutdownContainer().register((", AutoCloseable.class, ") ", instanceRef, ");");
             method.line("}");
         }
-    }
-
-    private static boolean validMethod(AnnotationProcessorContext context, ProviderDefinition providerDefinition, String destroyMethod) {
-        return providerDefinition.getAllProvidedTypes()
-                .stream()
-                .flatMap(type -> ProcessorUtils.allMethods(context, type).stream())
-                .anyMatch(method -> method.getSimpleName().toString().equals(destroyMethod) && method.getParameters().size() == 0);
     }
 }
