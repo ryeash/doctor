3965c2ac4f084fbf6a797ec9ae5de13b72b51faf
ryeash <ryan.yeash@broadcom.com>
2022-09-16 11:11:22 -0600

    wip

--- doctor-core/src/main/java/vest/doctor/aop/MethodMetadata.java
+++ doctor-core/src/main/java/vest/doctor/aop/MethodMetadata.java
@@ -6,7 +6,7 @@ import vest.doctor.TypeInfo;
 import java.util.List;
 
 /**
- * Static metadata about an invoked method that can be statically compiled.
+ * Metadata about an invoked method that can be statically compiled.
  */
 public record MethodMetadata(
         Object containingInstance,
